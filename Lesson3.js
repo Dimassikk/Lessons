e0be84bceadcb45534691cbac45d7fd7e8e33e01
Dimassikk <dimspavlenkoo@gmail.com>
2021-12-07 23:26:26 +0200

    Added Lesson3.js

---
+++ Lesson3.js
@@ -0,0 +1,6 @@
+var name = "Nickolas";
+console.log("Hello" + name);
+if (name.length > 7) {console.log("Wow,u have a really long name");
+} else {
+    console.log("You have short name,pidor!");
+}
