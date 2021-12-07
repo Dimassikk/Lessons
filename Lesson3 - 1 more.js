ff60ac5e4b6a816d776e620cbae9b272752e96ec
Dimassikk <dimspavlenkoo@gmail.com>
2021-12-07 23:37:28 +0200

    Added Lesson3 -  1 more.js

---
+++ Lesson3 - 1 more.js
@@ -0,0 +1,10 @@
+var lemonChikecken = false;
+var beefWithBlack = true;
+var sweetAndChtotoTam = true;
+if (lemonChikecken) {
+    console.log("Great, i have lemon chicken!!!!!Z!!!!!!");
+} else if (beefWithBlack) {
+console.log("OK, i have beef.");
+} else if (sweetAndChtotoTam); {
+    console.log("blyat,chtoto tam,ladno(((");
+}
