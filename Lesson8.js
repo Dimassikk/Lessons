0d1e3e674d0ba27da038e944cecc003f0279dffb
Dimassikk <dimspavlenkoo@gmail.com>
2021-12-22 20:22:28 +0200

    Added Lesson8.js

---
+++ Lesson8.js
@@ -0,0 +1,7 @@
+var ourFirstFunction = function () {
+    console.log("Hello world!");
+}; 
+ourFirstFunction();
+console.log(ourFirstFunction.toString());
+var test = "test string";
+console.log(test);
