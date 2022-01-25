15291dad93dd1af69d85f655385226339ee32f5e
Sergii Pavlenko <sergiipavlenko@v.stockx.com>
2022-01-25 21:27:44 +0200

    Added Lesson For-If

--- Lesson11.js
+++ Lesson11.js
@@ -1,18 +1,42 @@
-// Task 1
-var animals = ["Cat", "Fish", "Lemur", "Komodo Dragon"];
+// // Task 1
+// var animals = ["Cat", "Fish", "Lemur", "Komodo Dragon"];
 
-for (var i = 0; i < animals.length; i++ ) {
-    animals[i] = "Awesome " + animals[i];
+// for (var i = 0; i < animals.length; i++) {
+//     animals[i] = "Awesome " + animals[i];
+// }
+
+// console.log(animals.join());
+
+// // Task 2
+// var alphabet = "abcdefghijklmnopqrstuvwxyz";
+// var count = 0;
+// var randomString = "";
+// while (count < 6) {
+//     var temp = alphabet[Math.floor(Math.random() * alphabet.length)]; 
+//     console.log(temp);
+//     randomString += temp;
+//     count++;
+// }
+// console.log(randomString);
+
+// Task 3
+var input = "javascript is awesome";
+var output = "";
+
+for (var i = 0; i < input.length; i++) {
+    var temp = input[i];
+
+    if (temp === "a") {
+        // console.log("4");
+        temp = 4;
+    } else if (temp === "e") {
+        temp = 3;
+    } else if (temp === "i") {
+        temp = 1;
+    } else if (temp === "o") {
+        temp = 0;
+    }
+    console.log(temp);
+    output += temp;
 }
-
-console.log(animals.join());
-
-// Task 2
-var alphabet = "abcdefghijklmnopqrstuvwxyz";
-var count = 0;
-var randomString = "";
-while (count < 6) {
-    randomString += alphabet[Math.floor(Math.random() * alphabet.length)];
-    count++;
-}
-console.log(randomString);
+console.log(output);
